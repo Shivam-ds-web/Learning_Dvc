d64e66ad16345d2cc6abc5bee2d44d38855f0266
Shivam <dubeyshivam0005@gmail.com>
2026-05-29 16:45:22 +0530

    1 - first commit saved data

---
+++ code2.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import os
+data = {
+    'Name' : ['Charlie','Sam','John'],
+    'Salary' : [20000,30000,40000]
+}
+dataf = pd.DataFrame(data)
+os.makedirs('data2',exist_ok= True)
+file_path = os.path.join('data2','sample.csv')
+dataf.to_csv(file_path)
+print('file Saved successfully')
