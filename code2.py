6b0df652daa24cb96855f1300424ab988c4bf3bc
Shivam <dubeyshivam0005@gmail.com>
2026-05-31 11:45:10 +0530

    2nd version of data

--- code2.py
+++ code2.py
@@ -10,3 +10,8 @@ file_path = os.path.join('data2','sample.csv')
 dataf.to_csv(file_path)
 print('file Saved successfully')
 os.makedirs('S3',exist_ok=True)
+# makibng changes in data to check whether dvc is tracking data or not 
+new_row = {'Name' : 'Yash','Salary' : 50000}
+dataf.loc[len(dataf.index)] = new_row
+dataf.to_csv(file_path)
+print(dataf)
